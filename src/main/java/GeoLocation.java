206cd6811a322159b187f3cd54ed69705d1c4bc9
Wangyuxuan-xuan <you@example.com>
2021-02-26 12:03:12 +0100

    Add GeoLocation record class

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,16 @@
+public record GeoLocation() {
+    private static String as;
+    private static String city;
+    private static String country;
+    private static String countryCode;
+    private static String isp;
+    private static double lat;
+    private static double lon;
+    private static String org;
+    private static String query;
+    private static String region;
+    private static String regionName;
+    private static String timezone;
+    private static String zip;
+
+}
